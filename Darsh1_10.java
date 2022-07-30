3135ce3f58edbf1c782b6cf4b96acbfefffc3dc9
006Darsh <108823948+006Darsh@users.noreply.github.com>
2022-07-30 20:01:08 +0530

    Update Darsh1_10.java

--- Darsh1_10.java
+++ Darsh1_10.java
@@ -4,11 +4,14 @@ public class Darsh1_10 {
         StringBuffer result = new StringBuffer();
 
         for (int i = 0; i < text.length(); i++) {
-            if (Character.isUpperCase(text.charAt(i))) {
-                char ch = (char) (((int) text.charAt(i) + s - 65) % 26 + 65);
+            if (Character.isUpperCase(text.charAt(i))) //checks if the current char in the entered string is in uppercase or not.
+            {
+                char ch = (char) (((int) text.charAt(i) + s - 65) % 26 + 65);//if yes then it increases its ascii value by s numbers so the char changes
                 result.append(ch);
-            } else {
-                char ch = (char) (((int) text.charAt(i) + s - 97) % 26 + 97);
+            } 
+            else 
+            {
+                char ch = (char) (((int) text.charAt(i) + s - 97) % 26 + 97);//it increases its ascii value by s numbers so the char changes
                 result.append(ch);
             }
         }
