d2e1a3fd4e7fa97fa737fd9485c774d6d6d4b09c
006Darsh <108823948+006Darsh@users.noreply.github.com>
2022-07-30 18:35:39 +0530

    Update Darsh1_3.java

--- Darsh1_3.java
+++ Darsh1_3.java
@@ -6,11 +6,11 @@ public class Darsh1_3
     {
         int r1,r2;
         r1 = a%10;
-        r2 = b%10;
+        r2 = b%10;//taking remainder of both numbers.
         boolean p=true;
         if(r1 == r2)
         {
-            p=true;
+            p=true;//checking if the remainders are same the both have same last digit.
         }
         else if(r1!=r2)
         {
