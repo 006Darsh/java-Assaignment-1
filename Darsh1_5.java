8b6b917d94379f55edfeb02f0ef141662481277b
006Darsh <108823948+006Darsh@users.noreply.github.com>
2022-07-30 19:33:24 +0530

    Update Darsh1_5.java

--- Darsh1_5.java
+++ Darsh1_5.java
@@ -1,24 +1,20 @@
-
 import static java.lang.Math.min;
 
 public class Darsh1_5 {
-   
-    Darsh1_5(String x,String y)
-    {
-         // Figure which string is shorter.
+
+    Darsh1_5(String a, String b) {
+        // Figure which string is shorter.
         int len = Math.min(a.length(), b.length());
         int count = 0;
 
         // Look at both substrings starting at i
-        for (int i=0; i<len-1; i++) 
-        {
-             String aSub = a.substring(i, i+2);//taking substrings of both strings.
-             String bSub = b.substring(i, i+2);
-             if (aSub.equals(bSub)) 
-             {  // Use .equals() with strings
-               count++;
-             }
+        for (int i = 0; i < len - 1; i++) {
+            String aSub = a.substring(i, i + 2);// taking substrings of both strings.
+            String bSub = b.substring(i, i + 2);
+            if (aSub.equals(bSub)) { // Use .equals() with strings
+                count++;
+            }
         }
-        System.out.println("stringMatch(\""+x+"\", \""+y+"\") --> "+count);
+        System.out.println("stringMatch(\"" + x + "\", \"" + y + "\") --> " + count);
     }
 }
