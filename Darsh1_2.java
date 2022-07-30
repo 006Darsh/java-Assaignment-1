ce0b3a1f08d0abe87b7721e68908e5f50e237019
006Darsh <108823948+006Darsh@users.noreply.github.com>
2022-07-30 18:33:18 +0530

    Update Darsh1_2.java

--- Darsh1_2.java
+++ Darsh1_2.java
@@ -1,12 +1,12 @@
 
 import java.util.*;
 public class Darsh1_2 {
-    Darsh1_2(String a){
+    Darsh1_2(String a){//making an constructor of class file.
         if(a.charAt(0)=='O' || a.charAt(0)=='o')
         {
-            System.out.print(a.charAt(0));
+            System.out.print(a.charAt(0));//Checking that 'o' is at first bit or not.
         }
-        if(a.charAt(1)=='z' || a.charAt(1)=='Z')
+        if(a.charAt(1)=='z' || a.charAt(1)=='Z')//Checking that 'z' is at 2nd bit or not.
         {
             System.out.print(a.charAt(1));
         }
