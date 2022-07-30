efde1f717870a2991404319231d5f48bcbd6dbf7
006Darsh <108823948+006Darsh@users.noreply.github.com>
2022-07-30 15:39:21 +0530

    Add files via upload

---
+++ Darsh1_2main.java
@@ -0,0 +1,19 @@
+// Name :- Aswani Darsh
+// Roll-no :-21ce006
+// Aim :-Given a string, return a string made of the first 2 chars (if present), however include 
+// first char only if it is 'o' and include the second only if it is 'z', so "ozymandias" 
+// yields "oz".
+// startOz("ozymandias") → "oz"
+// startOz("bzoo") → "z"
+// startOz("oxx") → "o"
+// Git-hub repository: https://github.dev/006Darsh/java-Assaignment-1
+import java.util.*;
+public class Darsh1_2main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter String:");
+        String A=sc.nextLine();
+        Darsh1_2 d2 = new Darsh1_2(A);
+        sc.close();
+    }
+}
